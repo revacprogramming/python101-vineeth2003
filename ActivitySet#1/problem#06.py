951524148e13b29eef90531e82b5b55e7ef54f9f
Vineeth B <94664950+vineeth2003@users.noreply.github.com>
2022-05-26 09:26:35 +0000

    program 6

--- ActivitySet#1/problem#06.py
+++ ActivitySet#1/problem#06.py
@@ -13,4 +13,4 @@ for intervar in [3, 41, 2, 5, 74, 15]:
       if largest is  intervar > largest:
         largest = intervar
         print('Loop:', intervar, largest)
-print("Maximum", largest)
+print(" Maximum", largest)
