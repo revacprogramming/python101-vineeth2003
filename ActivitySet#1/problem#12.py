5490598615fabe0984d918bc28a9dd307255c601
Vineeth B <94664950+vineeth2003@users.noreply.github.com>
2022-06-30 09:17:04 +0000

    program 12

--- ActivitySet#1/problem#12.py
+++ ActivitySet#1/problem#12.py
@@ -1,3 +1,9 @@
 # Regular Expressions
 # https://www.py4e.com/lessons/regex
-.*@.*\. .*
+import re
+hand = open("dataset/romeo.txt")
+for line in hand:
+  line = line.rstrip()
+  x = re.findall('\S*[0-9]\S*', line)
+  if line(x)>0:
+    print(x)
