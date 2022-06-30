954d556f4c52cae9e37b173180bb22dcb7257452
Vineeth B <94664950+vineeth2003@users.noreply.github.com>
2022-06-30 10:14:23 +0000

    program 15

--- ActivitySet#1/problem#15.py
+++ ActivitySet#1/problem#15.py
@@ -1,2 +1,22 @@
 # Object Oriented Programming
 # https://www.py4e.com/lessons/Objects
+class Car:
+  Wheel = 6
+  def __init__(self):
+    self.mil = 10
+    self.company = 'Honda'
+
+c1 = Car()
+c2 = Car()
+
+#print(c1.company)
+
+c2.company = 'Maruthi'
+
+print(c1.company)
+print(c2.company)
+
+print('-------------------------')
+
+print(c1.Wheel)
+print(c2.Wheel)
