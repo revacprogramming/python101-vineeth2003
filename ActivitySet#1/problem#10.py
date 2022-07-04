ba19621f52e7f1c772e34976d38ba5676778871a
Vineeth B <94664950+vineeth2003@users.noreply.github.com>
2022-07-04 10:15:39 +0000

    program 10 again

--- ActivitySet#1/problem#10.py
+++ ActivitySet#1/problem#10.py
@@ -1,15 +1,18 @@
 # Dictionaries
+fname = input('enter the file name')
+try:
+  fhand = open(fname)
+except:
+  print('file cannot be opened: ', fname)
+  exit()
 
-counts = "Writing programs or programming is a very creative and rewarding activity  You can write programs for many reasons ranging from making your living to solving a difficult data analysis problem to having fun to helping someone else solve a problem  This book assumes that {\em everyone} needs to know how to program and that once you know how to program, you will figure out what you want to do with your newfound skills. We are surrounded in our daily lives with computers ranging from laptops to cell phones  We can think of these computers as our personal assistants who can take care of many things on our behalf  The hardware in our current-day computers is essentially built to continuously ask us the question.hat would you like me to do next. Our computers are fast and have vasts amounts of memory and could be very helpful to us if we only knew the language to speak to explain to the computer what we would like it to do next. If we knew this language we could tell the computer to do tasks on our behalf that were reptitive. Interestingly, the kinds of things computers can do best are often the kinds of things that we humans find boring and mind-numbing"
 counts = dict()
-print('enter a line of text: ')
-line = input('')
+for line in fhand:
+  words = line.split()
+  for word in words:
+    if word not in counts:
+      counts[word] = 1
+    else:
+      counts[word] += 1
 
-words = line.split()
-
-print('Words: ', words)
-
-print('counting....')
-for word in words:
-  counts[word] = counts.get(word,0) + 1
-print(Counts,counts)
+print(counts)
